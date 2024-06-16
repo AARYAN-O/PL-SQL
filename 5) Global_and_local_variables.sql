11b347739e0e50506cece9d08d69ba37bb43303f
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-16 21:46:28 +0530

    Create 5) Global_and_local_variables.sql

---
+++ 5) Global_and_local_variables.sql
@@ -0,0 +1,16 @@
+-- we use declare to declare the variables.So in case of both global and local variables , we will be having it.
+
+-- the below line represents global variable.
+DECLARE 
+	num1 number := 12;
+	num2 number := 14;
+
+BEGIN 
+	dbms_output.put_line(num1 || num2)
+    -- the below line represents local variable.
+	DECLARE 
+ 		num3 number := 16
+		num4 number := 18
+		dbms_output.put_line(num3 || num4)
+	END;
+END;
